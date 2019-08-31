2047869cd82b5ab3e8b375cef23fe0620290c4c9
Dexon Noxed <dexon@live.ca>
2019-08-30 20:42:26 -0400

    Build v1.3.0

---
+++ precache-manifest.f47dd41bcaa66579fd08b7b9f3e9196f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89de1ad42eed6b55d09e73dfdb64a389",
+    "url": "/index.html"
+  },
+  {
+    "revision": "806d16cf900d2b808861",
+    "url": "/static/css/2.3fc73323.chunk.css"
+  },
+  {
+    "revision": "806d16cf900d2b808861",
+    "url": "/static/js/2.9f2ce911.chunk.js"
+  },
+  {
+    "revision": "785869c98b1eb1155504",
+    "url": "/static/js/main.5ea66b0b.chunk.js"
+  },
+  {
+    "revision": "57d944cef138818ac21d",
+    "url": "/static/js/runtime~main.5ae0f385.js"
+  }
+]);
